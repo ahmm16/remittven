889f18e38321bb16a51ac91d381a8bcd8fdb127c
Alex <39168831+ahmm16@users.noreply.github.com>
2020-03-24 21:30:34 +0100

    feat(new changes): changes 24/03
    -- Segundos cambios del 24/03

--- js/scripts.js
+++ js/scripts.js
@@ -62,30 +62,48 @@
         for (let item in data) {
             rate = data[item].result.rate.home_delivery["#text"]
         }
-        document.getElementById("rate").innerHTML = rate;
+        document.getElementById("rateVal").innerHTML = rate;
+        document.getElementById("rate").innerHTML = new Intl.NumberFormat("de-DE").format(rate);
         //document.getElementById("rate").innerHTML = 11.6379;
     }
     let template = $("body").hasClass("tpl-home")
     template && fetchUrl();
 
     function conversor(name, value) {
-        let rate = parseInt($("#rate").text());
+        let rate = parseInt($("#rateVal").text());
         value = value !== '' ? parseFloat(value) : 0
         if (value === 0) {
             $("#destination").val("0")
         }
         else {
-            $("#destination").val(value * rate)
+            $("#destination").val(new Intl.NumberFormat("de-DE").format(value * rate));
+            //$("#source").val(new Intl.NumberFormat("de-DE").format(value));
         }
     }
 
     $("#source").on("keyup", function (e) {
         let isNumber = /^\d*\.?\d*$/.test(e.target.value);
+
         isNumber ?
             conversor(e.target.name, e.target.value)
             :
             $("#destination").val("Introduce números sólo")
+
+
     });
+
+    $("#source").on("keyup", function (e) {
+        let isNumber = /^\d*\.?\d*$/.test(e.target.value);
+
+        isNumber ?
+            conversor(e.target.name, e.target.value)
+            :
+            $("#destination").val("Introduce números sólo")
+
+
+    });
+
+
     //======================
     // Sticky menu
     //======================
@@ -109,7 +127,76 @@
         $("body")
             .delay(500)
             .css({ overflow: "visible" });
+        $("#signUpName").val(getAllUrlParams().name.replace(/[.*+?^${}()|[\]\\]/g, " "));
+        $("#signUpLastName").val(getAllUrlParams().lastname.replace(/[.*+?^${}()|[\]\\]/g, " "));
+        $("#email").val(getAllUrlParams().email.replace(/[*+?^${}()|[\]\\]/g, " ").replace("%40", "@"));
+
     });
+
+    function getAllUrlParams(url) {
+
+        // get query string from url (optional) or window
+        var queryString = url ? url.split('?')[1] : window.location.search.slice(1);
+
+        // we'll store the parameters here
+        var obj = {};
+
+        // if query string exists
+        if (queryString) {
+
+            // stuff after # is not part of query string, so get rid of it
+            queryString = queryString.split('#')[0];
+
+            // split our query string into its component parts
+            var arr = queryString.split('&');
+
+            for (var i = 0; i < arr.length; i++) {
+                // separate the keys and the values
+                var a = arr[i].split('=');
+
+                // set parameter name and value (use 'true' if empty)
+                var paramName = a[0];
+                var paramValue = typeof (a[1]) === 'undefined' ? true : a[1];
+
+                // (optional) keep case consistent
+                paramName = paramName.toLowerCase();
+                if (typeof paramValue === 'string') paramValue = paramValue.toLowerCase();
+
+                // if the paramName ends with square brackets, e.g. colors[] or colors[2]
+                if (paramName.match(/\[(\d+)?\]$/)) {
+
+                    // create key if it doesn't exist
+                    var key = paramName.replace(/\[(\d+)?\]/, '');
+                    if (!obj[key]) obj[key] = [];
+
+                    // if it's an indexed array e.g. colors[2]
+                    if (paramName.match(/\[\d+\]$/)) {
+                        // get the index value and add the entry at the appropriate position
+                        var index = /\[(\d+)\]/.exec(paramName)[1];
+                        obj[key][index] = paramValue;
+                    } else {
+                        // otherwise add the value to the end of the array
+                        obj[key].push(paramValue);
+                    }
+                } else {
+                    // we're dealing with a string
+                    if (!obj[paramName]) {
+                        // if it doesn't exist, create property
+                        obj[paramName] = paramValue;
+                    } else if (obj[paramName] && typeof obj[paramName] === 'string') {
+                        // if property does exist and it's a string, convert it to an array
+                        obj[paramName] = [obj[paramName]];
+                        obj[paramName].push(paramValue);
+                    } else {
+                        // otherwise add the property
+                        obj[paramName].push(paramValue);
+                    }
+                }
+            }
+        }
+
+        return obj;
+    }
 
     //======================
     // Mobile menu
